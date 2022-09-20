3d2a7124f564ff6b6059b1dd87909912b2e463e2
Alexander Zhang <awz2106@columbia.edu>
2022-09-20 13:37:30 -0700

    Complete Color Mode and Rainbow Modes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,70 @@
-let currentColor = "#333333";
+const DEFAULT_COLOR = "#333333";
+const DEFAULT_MODE = "color";
+const DEFAULT_SIZE = 16;
 
-function createGrid() {
-  let container = document.getElementById("container");
-  let x = 5;
-  document.documentElement.style.setProperty("--columns-row", x);
-
-  for (let i = 0; i < x ** 2; i++) {
-    let div = document.createElement("div");
-    container.appendChild(div);
-    div.addEventListener("mousedown", function () {
-      this.style.backgroundColor = currentColor;
-    });
-  }
-}
-createGrid();
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
 
 function setCurrentColor(newColor) {
   currentColor = newColor;
 }
 
+function setCurrentMode(newMode) {
+  currentMode = newMode;
+}
+
+function setCurrentSize(newSize) {
+  currentSize = newSize;
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function createGrid(size) {
+  document.documentElement.style.setProperty("--columns-row", size);
+  for (let i = 0; i < size ** 2; i++) {
+    let div = document.createElement("div");
+    grid.appendChild(div);
+    div.addEventListener("mousedown", draw);
+    div.addEventListener("mouseover", draw);
+  }
+}
+
+function draw(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (currentMode === "color") {
+    e.target.style.backgroundColor = currentColor;
+  } else if (currentMode === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  }
+}
+
 const colorPicker = document.getElementById("colorInput");
+const colorBtn = document.getElementById("colorBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("clearBtn");
+const grid = document.getElementById("grid");
+
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode("color");
+rainbowBtn.onclick = () => setCurrentMode("rainbow");
+clearBtn.onclick = () => reloadGrid();
+
+function reloadGrid() {
+  clearGrid();
+  createGrid(currentSize);
+}
+
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE);
+};
